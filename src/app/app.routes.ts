6f8116d43faf52352bc04fce18f1edf7a5889530
ShaymaaEssa <flowrathesky@gmail.com>
2026-06-24 20:22:53 +0300

    add google font: Inter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { pages } from './core/environment/pages';
+import { SignupComponent } from './pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: pages.Signup, component: SignupComponent },
+];
