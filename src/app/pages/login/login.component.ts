291edc8f41cbe1f80baca6280c8ca56b22963d28
ShaymaaEssa <flowrathesky@gmail.com>
2026-06-26 07:31:04 +0300

    wire loginpage with its auth service

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { AuthenticationService } from '../../core/services/auth-service/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +15,38 @@ import { RouterLink } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   showPassword = false;
+
+  private readonly authService = inject(AuthenticationService);
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.loginForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required]),
+    });
+  }
+
   submitForm() {
-    // Handle form submission logic here
+    if (this.loginForm.invalid) {
+      alert('Fix Form Problem!');
+      return;
+    }
+
+    this.authService.sendLoginForm(this.loginForm.value).subscribe({
+      next: (response) => {
+        console.log('Login successful:', response);
+        alert('Login successful!');
+      },
+      error: (err) => {
+        console.error('Login failed:', err);
+        alert('Login failed. Please try again.');
+      },
+    });
   }
 }
