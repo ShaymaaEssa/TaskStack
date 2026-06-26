1055092e313134263973dca470f3608be0683e54
ShaymaaEssa <flowrathesky@gmail.com>
2026-06-26 07:25:54 +0300

    login page design

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm!: FormGroup;
+  showPassword = false;
+  submitForm() {
+    // Handle form submission logic here
+  }
+}
